de929b1d0e786a2c86b16a4a986cb179f08bb83a
jHapy Lead Dev <jhapy@jhapy.org>
2020-06-17 10:17:31 +0100

    Logback configuration Cache configuration Javers configuration

--- src/main/java/org/jhapy/baseserver/config/JaversBaseSqlConfiguration.java
+++ src/main/java/org/jhapy/baseserver/config/JaversBaseSqlConfiguration.java
@@ -20,6 +20,7 @@ package org.jhapy.baseserver.config;
 
 import javax.servlet.http.HttpServletRequest;
 import org.javers.spring.auditable.AuthorProvider;
+import org.jhapy.commons.config.Constants;
 import org.jhapy.commons.security.SecurityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,20 +38,6 @@ public class JaversBaseSqlConfiguration {
 
   @Bean
   public AuthorProvider authorProvider() {
-    return () -> {
-      String currentUsername = "Unknown";
-      try {
-        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
-        if (requestAttributes instanceof ServletRequestAttributes) {
-          HttpServletRequest servletRequest = ((ServletRequestAttributes) requestAttributes)
-              .getRequest();
-          currentUsername = servletRequest.getHeader("X-SecUsername");
-        } else {
-          currentUsername = SecurityUtils.getUsername();
-        }
-      } catch (IllegalStateException e) {
-      }
-      return currentUsername;
-    };
+    return () -> SecurityUtils.getCurrentUserLogin().orElse(Constants.ANONYMOUS_USER);
   }
 }
