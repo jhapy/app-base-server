b1438bd83029d2895cc7da6e2bd8267809031aa6
jHapy Lead Dev <jhapy@jhapy.org>
2020-12-01 11:26:00 +0100

    Add an update method, but still not working

--- src/main/java/org/jhapy/baseserver/service/CrudRelationalService.java
+++ src/main/java/org/jhapy/baseserver/service/CrudRelationalService.java
@@ -19,17 +19,25 @@
 package org.jhapy.baseserver.service;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import org.apache.commons.lang3.StringUtils;
 import org.jhapy.baseserver.domain.relationaldb.BaseEntity;
+import org.jhapy.baseserver.exception.ServiceException;
 import org.jhapy.commons.security.SecurityUtils;
+import org.jhapy.commons.utils.BeanUtils;
 import org.jhapy.commons.utils.HasLogger;
+import org.jhapy.commons.utils.OrikaBeanMapper;
 import org.jhapy.dto.domain.exception.EntityNotFoundException;
+import org.json.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -43,7 +51,6 @@ import org.springframework.transaction.annotation.Transactional;
  * @since 2019-03-26
  */
 public interface CrudRelationalService<T extends BaseEntity> extends HasLogger {
-
   JpaRepository<T, Long> getRepository();
 
   EntityManager getEntityManager();
@@ -88,10 +95,10 @@ public interface CrudRelationalService<T extends BaseEntity> extends HasLogger {
     if (previousValue == null && currentValue == null) {
       return false;
     }
-    if (previousValue == null && currentValue != null) {
+    if (previousValue == null) {
       return true;
     }
-    if (previousValue != null && currentValue == null) {
+    if (currentValue == null) {
       return true;
     }
     if (previousValue instanceof Number) {
@@ -212,4 +219,26 @@ public interface CrudRelationalService<T extends BaseEntity> extends HasLogger {
 
   CriteriaQuery buildSearchQuery(CriteriaQuery query, Root<T> entity, CriteriaBuilder cb,
       String currentUserId, String filter, Boolean showInactive, Object... otherCriteria);
+
+  default T update(OrikaBeanMapper mapperFacade, Map<String, Object> entity)
+      throws ServiceException {
+    if (!entity.containsKey("id") || entity.get("id") == null) {
+      throw new ServiceException("At least an ID attribute is required with a value");
+    }
+
+    long id;
+    try {
+      id = Long.parseLong(entity.get("id").toString());
+    } catch (NumberFormatException nfe) {
+      throw new ServiceException("ID attribute value is invalid");
+    }
+    T existingRecord = getRepository().findById(id).get();
+    convert(entity, existingRecord);
+
+    return save(existingRecord);
+  }
+
+  default void convert(Map<String, Object> entity, T existingRecord) {
+    BeanUtils.copyNonNullProperties(existingRecord, entity);
+  }
 }
