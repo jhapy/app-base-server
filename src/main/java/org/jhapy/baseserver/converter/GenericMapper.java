cb6f8feba2540ce804e0393c90287010db305549
jHapy Lead Dev <jhapy@jhapy.org>
2021-09-15 12:50:32 +0100

    changes

--- src/main/java/org/jhapy/baseserver/converter/GenericMapper.java
+++ src/main/java/org/jhapy/baseserver/converter/GenericMapper.java
@@ -7,19 +7,19 @@ import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Context;
 import org.mapstruct.Mapping;
 
-public abstract class GenericMapper<E, D> extends BaseConverterV2 {
+public abstract class GenericMapper<ENTITY, DTO> extends BaseConverterV2 {
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "created", ignore = true)
   @Mapping(target = "createdBy", ignore = true)
   @Mapping(target = "modified", ignore = true)
   @Mapping(target = "modifiedBy", ignore = true)
-  public abstract E asEntity(D dto, @Context Map<String, Object> context);
+  public abstract ENTITY asEntity(DTO dto, @Context Map<String, Object> context);
 
-  public abstract D asDTO(E entity, @Context Map<String, Object> context);
+  public abstract DTO asDTO(ENTITY entity, @Context Map<String, Object> context);
 
-  public abstract List<E> asEntityList(Iterable<D> dtoList, @Context Map<String, Object> context);
+  public abstract List<ENTITY> asEntityList(Iterable<DTO> dtoList, @Context Map<String, Object> context);
 
-  public abstract List<D> asDTOList(Iterable<E> entityList, @Context Map<String, Object> context);
+  public abstract List<DTO> asDTOList(Iterable<ENTITY> entityList, @Context Map<String, Object> context);
 
   public List<String> map( String value ) {
     if (StringUtils.isNotBlank(value))
